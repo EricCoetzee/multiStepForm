84f2150abe7f28f8715789c8be25ff5f5741f0dd
EricCoetzee <ericcoetzee123@gmail.com>
2021-10-30 13:49:51 +0200

    form add images

--- users/ajax/index.php
+++ users/ajax/index.php
@@ -1,7 +1,6 @@
 <?php include_once('../../includes/db/init.php') ?>
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
@@ -11,7 +10,6 @@
     <script src="../../dist/scripts/customSelect.js" defer></script>
     <title>Registraion Form</title>
 </head>
-
 <body>
     <form action="#" class="form">
         <h1 class="text-center">Create Post</h1>
@@ -27,7 +25,7 @@
         <!-- Steps -->
         <div class="form-step form-step-active">
             <div class="category-container">
-                <h3>Scroll for more categories</h3>
+                <label>Scroll for more categories</label>
                 <select class="cat-select__selected" size="5">
                     <?php
                     $db = new Database();
@@ -45,7 +43,7 @@
         <div class="form-step">
             <div class="form-step__grid-setup">
                 <div>
-                    <h3>Gender</h3>
+                    <label for="email">Gender</label>
                     <select class="cat-select__selected" size="4">
                         <?php
                         $db = new Database();
@@ -57,7 +55,7 @@
                     </select>
                 </div>
                 <div>
-                    <h3>Age Group</h3>
+                    <label for="email">Age Group</label>
                     <select class="cat-select__selected" size="4">
                         <?php
                         $db = new Database();
@@ -69,24 +67,8 @@
                     </select>
                 </div>
             </div>
-
             <div>
-                <input type="url" name="post-title" placeholder="Title" id="" class="form-input">
-            </div>
-
-            <div class="btns-group">
-                <a href="#" class="btn btn-prev">Previous</a>
-                <a href="#" class="btn btn-next">Next</a>
-            </div>
-        </div>
-        <div class="form-step">
-            <div class="input-group">
-                <label for="phone">Phone</label>
-                <input type="text" name="phone" id="phone" />
-            </div>
-            <div class="input-group">
-                <label for="email">Email</label>
-                <input type="text" name="email" id="email" />
+                <input type="url" name="post-title" placeholder="Youtube URL" id="" class="form-input">
             </div>
             <div class="btns-group">
                 <a href="#" class="btn btn-prev">Previous</a>
@@ -95,12 +77,52 @@
         </div>
         <div class="form-step">
             <div class="input-group">
-                <label for="dob">Date of Birth</label>
-                <input type="date" name="dob" id="dob" />
+                <label for="title">Title</label>
+                <input type="text" name="title" id="title" />
             </div>
             <div class="input-group">
-                <label for="ID">National ID</label>
-                <input type="number" name="ID" id="ID" />
+                <label for="description">Description</label>
+                <textarea type="text" name="description" id="description" class="input-group__post-description" placeholder="description"></textarea>
+            </div>
+            <div class="btns-group">
+                <a href="#" class="btn btn-prev">Previous</a>
+                <a href="#" class="btn btn-next">Next</a>
+            </div>
+        </div>
+        <div class="form-step">
+            <label>Add Images</label>
+            <div class="form-step__img-upload-section">
+                <?php
+                for ($x = 0; $x <= 3; $x++) {
+                ?>
+                    <div class="image-upload-<?php echo $x; ?>">
+                        <div class="center">
+                            <div class="form-input">
+                                <label for="file-ip-<?php echo $x; ?>">
+                                    <img id="file-ip-<?php echo $x; ?>-preview" alt="__Add Image" src="../../img/image.png">
+                                    <button type="button" class="imgRemove" onclick="myImgRemove(<?php echo $x; ?>)"></button>
+                                </label>
+                                <input type="file" name="img<?php echo $x; ?>"  id="file-ip-<?php echo $x; ?>" accept="image/*" onchange="showPreview(event, <?php echo $x; ?>);">
+                            </div>
+                        </div>
+                    </div>
+                    <script>
+                        var number = <?php echo $x;?>;
+                        console.log(<?php echo $x;?>)
+                            function showPreview(event, number) {
+                                if (event.target.files.length > 0) {
+                                    let src = URL.createObjectURL(event.target.files[0]);
+                                    let preview = document.getElementById("file-ip-" + number + "-preview");
+                                    preview.src = src;
+                                    preview.style.display = "block";
+                                }
+                            }
+                            function myImgRemove(number) {
+                                document.getElementById("file-ip-" + number + "-preview").src = "../../img/image.png";
+                                document.getElementById("file-ip-" + number).value = null;
+                            }
+                    </script>
+                <?php } ?>
             </div>
             <div class="btns-group">
                 <a href="#" class="btn btn-prev">Previous</a>
@@ -123,5 +145,4 @@
         </div>
     </form>
 </body>
-
 </html>
