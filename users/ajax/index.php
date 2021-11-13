bdedf3bcfb8876dc86af541e3c7c6aae85564b7d
EricCoetzee <ericcoetzee123@gmail.com>
2021-11-13 02:07:51 +0200

    update I guess

--- users/ajax/index.php
+++ users/ajax/index.php
@@ -6,8 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" href="../../dist/css/multi_form.css" />
-    <script src="../../dist/scripts/mulForm.js" defer></script>
-    <script src="../../dist/scripts/customSelect.js" defer></script>
+    
     <title>Registraion Form</title>
 </head>
 <body>
